0fefb72cf28fb762d2adc27a4475399b0187f16b
Mateo <mateopuna98@gmail.com>
2019-12-28 19:50:03 -0400

    added tmpHigh and tmpLow

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain and a high of ' + body.daily.data[0].temperatureHigh + ' and a low of ' + body.daily.data[0].temperatureLow)
         }
     })
 }
